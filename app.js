b0d07f3a1589097e79d7858241bb0e1637f54bca
perfoo <perfoo@yahoo.com>
2021-09-14 18:27:21 +0200

    uvjeti-pravila

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ let datum = new Date();
 let year = datum.getFullYear();
 
 document.getElementById("footer-date").innerHTML = `&copy; OTKUP MOBITELA ZAGREB - ${year}`;
+
+function showP() {
+  var x = document.getElementById("hide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
