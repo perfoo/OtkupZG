2374833ec0e3bd373b8be894bf4d84bc1d7a97a6
perfoo <perfoo@yahoo.com>
2021-09-15 14:01:03 +0200

    js+button

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 let datum = new Date();
 let year = datum.getFullYear();
-
 document.getElementById("footer-date").innerHTML = `&copy; OTKUP MOBITELA ZAGREB - ${year}`;
 
 function showP() {
-  var x = document.getElementById("hide");
+  const x = document.getElementById("hide");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
 } 
+
+const endpoint = "https://api.hnb.hr/tecajn/v2";
+const body = document.querySelector("body");
+
+fetch(endpoint)
+  .then((response) => {
+    return response.json();
+  })
+
+  .then((data) => {
+    console.log(`${Valuta}`);
+  })
+
+  .catch((error) => {
+    console.log(error);
+  });
